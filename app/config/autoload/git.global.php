48ce991d6dcdca97f69a5428313cdcf599c4f0d8
Benjamin Hirsch <mail@benjaminhirsch.net>
2026-07-02 15:56:51 +0200

    git related configuration can now be configured via env variable

--- app/config/autoload/git.global.php
+++ app/config/autoload/git.global.php
@@ -2,12 +2,18 @@
 
 declare(strict_types=1);
 
+$repositoriesPath   = getenv('TRAGWERK_REPOSITORIES_PATH') ?: 'data/repositories';
+$authorizedKeysPath = getenv('TRAGWERK_AUTHORIZED_KEYS_PATH') ?: 'data/ssh/authorized_keys';
+$sshHost            = getenv('TRAGWERK_SSH_HOST') ?: 'tragwerk-git';
+$sshRepoBase        = getenv('TRAGWERK_SSH_REPO_BASE') ?: 'repos';
+$appInternalUrl     = getenv('TRAGWERK_APP_INTERNAL_URL') ?: 'http://app';
+
 return [
     'git' => [
-        'repositories_path'    => 'data/repositories',
-        'authorized_keys_path' => 'data/ssh/authorized_keys',
-        'ssh_host'             => 'tragwerk-git',
-        'ssh_repo_base'        => 'repos',
-        'app_internal_url'     => 'http://app',
+        'repositories_path'    => $repositoriesPath,
+        'authorized_keys_path' => $authorizedKeysPath,
+        'ssh_host'             => $sshHost,
+        'ssh_repo_base'        => $sshRepoBase,
+        'app_internal_url'     => $appInternalUrl,
     ],
 ];
